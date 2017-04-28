44d9af58df4620f2e1ea6af13860924f921dcd75
Tiago Oliveira <tiago@econtabil.com>
2017-04-27 22:08:00 -0300

    commitando...

--- src/com/opet/Main.java
+++ src/com/opet/Main.java
@@ -1,15 +1,51 @@
 package com.opet;
 
+
+import com.opet.util.Reader;
+
 public class Main
 {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
-        // TODO Auto-generated method stub
-        while(true) {
+        int op;
+        while(op != 0) {
+            System.out.println("Digite a opção desejada:");
+            System.out.println("1 - Incluir novo aluno");
+            System.out.println("2 - Excluir aluno");
+            System.out.println("3 - Listar alunos");
+
+            int opcao = Reader.readInt();
+
+            switch (opcao)
+            {
+                case 1:
+                    System.out.println("Cadastrando aluno.");
+                    System.out.println("Digite o código:");
+                    int alunoCodigo = Reader.readInt();
+                    System.out.println("Digite o nome:");
+                    String alunoNome = Reader.readString();
+
+
+                    break;
+                case 2:
+                    System.out.println("Excluir aluno.");
+                    System.out.println("Digite o código do aluno.");
+                    break;
+
+                default:
+                    break;
+            }
 
         }
 
+    }
+
+    public static Aluno inserirAluno(String []tiago, int Codigo, String nome)
+    {
+        Aluno alunoRegistro = new Aluno(nomeAluno, cpfAluno, sexoAluno);
+
+        return alunoRegistro;
     }
 
 }
