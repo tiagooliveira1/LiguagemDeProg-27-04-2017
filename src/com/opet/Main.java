20643358b1afcc6042baaa1abf262b5188e2a4c3
Tiago Oliveira <tiago@econtabil.com>
2017-05-04 19:52:50 -0300

    Ajuste de código

--- src/com/opet/Main.java
+++ src/com/opet/Main.java
@@ -8,7 +8,8 @@ public class Main
 
     public static void main(String[] args) throws Exception
     {
-        int op;
+        int op = -1;
+        Aluno []alunos = new Aluno[2];
         while(op != 0) {
             System.out.println("Digite a opção desejada:");
             System.out.println("1 - Incluir novo aluno");
@@ -25,12 +26,17 @@ public class Main
                     int alunoCodigo = Reader.readInt();
                     System.out.println("Digite o nome:");
                     String alunoNome = Reader.readString();
+                    alunos[alunos.length-1] = inserirAluno(alunoCodigo, alunoNome);
 
 
                     break;
                 case 2:
                     System.out.println("Excluir aluno.");
                     System.out.println("Digite o código do aluno.");
+                    break;
+                case 3:
+                    System.out.println("== Listando alunos == .");
+                    listAlunos(alunos);
                     break;
 
                 default:
@@ -41,11 +47,23 @@ public class Main
 
     }
 
-    public static Aluno inserirAluno(String []tiago, int Codigo, String nome)
+    public static Aluno inserirAluno(int Codigo, String nome)
     {
-        Aluno alunoRegistro = new Aluno(nomeAluno, cpfAluno, sexoAluno);
+        Aluno alunoRegistro = new Aluno(nome, Codigo, "");
 
         return alunoRegistro;
+    }
+
+    public static void listAlunos(Aluno []arrAlunos)
+    {
+        System.out.println("");
+        System.out.println("Listagem de alunos inseridos");
+        System.out.println("-----------------------------");
+        for (int i=0;i<arrAlunos.length;i++) {
+            System.out.println(arrAlunos[i].nome);
+        }
+        System.out.println("");
+
     }
 
 }
